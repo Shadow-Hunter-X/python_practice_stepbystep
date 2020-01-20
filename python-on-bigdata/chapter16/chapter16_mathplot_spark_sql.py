9618cc24f288a25a526d451a817c41ae49003d8f
J20 <LYJ__h@163.com>
2020-01-20 16:18:07 +0800

    add chapter 14,15,16

---
+++ python-on-bigdata/chapter16/chapter16_mathplot_spark_sql.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+sc=SparkContext(appName="spark sql mathplotlib")          
+spark=SparkSession.builder.appName('spark sql mathplotlib').master("Yct201811021847").getOrCreate()
+
+movies_df=spark.read.csv('movies.csv',header=True, inferSchema=True).dropna()
+ratings_df=spark.read.csv('ratings.csv',header=True,inferSchema=True).dropna()
+movies_df.createOrReplaceTempView('movies')      
+ratings_df.createOrReplaceTempView('ratings')
+
+data=spark.sql(" SELECT title , rating , userID, genres ,avg_movie_rating , DENSE_RANK() OVER( ORDER BY avg_movie_rating desc) rank FROM \
+    (SELECT m.title , rating , userID, m.genres, \
+    AVG(rating) OVER(partition by m.movieId) avg_movie_rating \
+    FROM  movies m LEFT JOIN ratings r ON r.movieId = m.movieId \
+    WHERE rating is not null ) A ")
+
+data.createOrReplaceTempView('data')
+
+
+users=spark.sql(" select userID , count(*) cnt from data where rank='1' group by userID").filter('cnt > 4')
+user_pd=users.toPandas()
+userID=user_pd.userID.to_list()
+user_cnt = user_pd.cnt.to_list()
+len(user_cnt)
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+fig.set_figheight(7)
+fig.set_figwidth(7)
+
+ax1 = fig.add_subplot(211)
+ax1.scatter(np.arange(9),user_cnt)
+ax1.set_xticklabels(user_cnt,rotation=45)
+
+ax2 = fig.add_subplot(212)
+ax2.bar(np.arange(9),user_cnt)
+ax2.set_xticklabels(user_cnt,rotation=45)
+
+#ax3 = fig.add_subplot(313)
+#ax3.bar(len(user_cnt),user_cnt)
+#ax3.set_xticklabels(userID,rotation=45)
+
+
+
+
---
+++ python-on-bigdata/chapter16/chapter16_目录.md
@@ -0,0 +1,30 @@
+[WordCloud - 标签云-使用方式](./chapter16_wordcloud.ipynb)  
+
+[WordCloud - 标签云-hdfs文件](./chapter16_wordcloud_hdfs.ipynb)  
+
+[WordCloud - 标签云-Hive操作](./chapter16_wordcloud_hdfs.ipynb)  
+
+[WordCloud - 标签云-PySpark操作](./chapter16_wordcloud_hdfs.ipynb)  
+
+[WordCloud - 标签云-PySpark操作](./chapter16_wordcloud_hdfs.ipynb)  
+
+[zeppelin的使用](./chapter16_zeppelin.json)  
+
+![查询可可视化效果图](./16-8.png)
+![查询可可视化效果图](./16-9.png)
+
+[mathplotlib pyplot使用](./chapter16_mathplotlib_pyplot.ipynb)  
+
+[mathplotlib Python方式使用](./chapter16_mathplotlib_python.py)  
+
+![效果图](./16-12.png)
+
+[mathplotlib Pyspark ml操作](./chapter16_mathplot_pyspark_ml.ipynb)  
+
+[mathplotlib Pyspark SQL操作](./chapter16_mathplot_spark_sql.py)  
+
+[Superset操作](./chapter16_northwind_dashboard.pickle)  
+
+![Superset Dashboard效果图](./16-23.png)
+
+
